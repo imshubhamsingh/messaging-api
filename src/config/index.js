8a6d2f2f386d0601c2acaf97cb60bdf6c3d41c08
Shubham Singh <imshubhamsingh97@gmail.com>
2018-03-24 18:23:12 +0530

    db path in env

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 30105;
 export const LIMIT = "100kb";
-export const MONGOURL = "mongodb://localhost:27015/movie-api";
+export const MONGOURL = process.env.DB;
 export const SECRET = process.env.SECRET;
 export const TOKENTIME = process.env.TOKENTIME; //30 days
 export const SALT = process.env.SALT;
