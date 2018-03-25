a4c973c21cddb31a932e41e797d71d966c4f52b8
Shubham Singh <imshubhamsingh97@gmail.com>
2018-03-25 15:42:02 +0530

    fixing heroku deploy with mongo atlas database

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import { MONGOURL } from "./config";
 mongoose.Promise = Promise;
 
 export default callback => {
-  let db = mongoose.connect(MONGOURL, (err, res) => {
+  let db = mongoose.connect(MONGOURL, { uri_decode_auth: true }, (err, res) => {
     if (err) {
       console.log("ERROR connecting to: " + MONGOURL + ". " + err);
     } else {
